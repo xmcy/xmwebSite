472cafc7223576fd5f6d7f6c2ad2776c27220d9e
xmcy <1195301507@qq.com>
2017-09-07 12:02:47 +0800

    首页展示

--- public/dist/js/partner.js
+++ public/dist/js/partner.js
@@ -1,152 +1,16 @@
 $(document).ready(function(){
-    $(".forwebnav").on("mouseenter",function () {
-        $(this).addClass("forwenavstyle");
-        $(".webnav").show();
-    })
-    $(".webnav").on("mouseenter",function () {
-        $(".webnav").show();
-        $(".forwebnav").addClass("forwenavstyle");
-    })
-    $(".webnav").on("mouseleave",function () {
-        $(".webnav").hide();
-        $(".forwebnav").removeClass("forwenavstyle");
-    })
-    $(".submmint").on("click",function () {
-        var    address=$(".address").val();
-        var    companyName=$(".companyName").val();
-        var    contactorName=$(".contactorName").val();
-        var    contactorNum=$(".contactorNum").val();
-        var    contactorWay=$(".contactorWay").val();
-        var    restmobible = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
-        var    testmobile=/^([0-9]{3,4}-)?[0-9]{7,8}$/;
-        if(address.trim()==""){
-				swal({
-					title: "所在城市不能为空",
-					type: "error",
-					confirmButtonText: "确定"
-				});
-				return false;
-			}
-        if(companyName.trim()==""){
-            swal({
-                title: "企业名称不能为空",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if(companyName.trim()!==""&&companyName.length>=30){
-            swal({
-                title: "企业名称不能超过30个字",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if(contactorName.trim()==""){
-            swal({
-                title: "联系人不能为空",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if(contactorName.trim()!==""&&companyName.length>=30){
-            swal({
-                title: "联系人名字不能超过30个字",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if(contactorNum.trim()==""){
-            swal({
-                title: "联系电话不能为空",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if (restmobible.test(contactorWay)||testmobile.test(contactorWay)){
-		}else {
-            swal({
-                title: "联系电话填写有误！",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        if(contactorWay.trim()==""){
-            swal({
-                title: "公司地址不能为空",
-                type: "error",
-                confirmButtonText: "确定"
-            });
-            return false;
-        }
-        var data={
-            address:address,
-            companyName:companyName,
-            contactorName:contactorName,
-            contactorNum:contactorNum,
-            contactorWay:contactorWay,
-        }
-        swal({
-                title: "确认提交",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "确定",
-                closeOnConfirm: false
-            },
-            function(){
-                $.ajax({
-                    url:"/users/",
-                    data:JSON.stringify(data),
-                    type:"POST",
-                    dataType:"json",
-                    contentType:"application/json",
-                    success:function(res){
-                        console.log(res)
-                        if(res.result=="success"){
-                            swal({
-                                title: "提交成功!",
-                                type: "success",
-                                confirmButtonText: "确定"
-                            });
-                            window.location.reload();
-                        }else {
-                            swal({
-                                title: "提交失败!",
-                                type: "error",
-                                confirmButtonText: "确定"
-                            });
-                        }
-                    },
-                    error:function (res) {
-                        swal({
-                            title: "提交失败!",
-                            type: "error",
-                            confirmButtonText: "确定"
-                        });
-                        console.log("error"+res);
-                    }
-                })
-
-            });
-    })
     $('#fullPage').fullpage({
         verticalCentered: false,
         //sectionsColor: ['aqua','crimson','green','darkviolet'],
         anchors: ['page1','page2','page3','page4','page5','page6','page7'],
         navigation: true,
-        navigationTooltips: ['制作宝，它终于来了','真正与你贴近的广告制作服务平台','制作宝平台优势','城市运营商合作模式','城市运营商激励模式','我们需要这样的你','加入我们'],
+        navigationTooltips: ['制作宝，它终于来了','真正与你贴近的广告制作服务平台','制作宝平台优势','城市合伙人合作模式','城市合伙人激励模式','我们需要这样的你','加入我们'],
         //滚动到某一屏后产生的动画效果
         afterLoad: function(link, index){
             switch (index){
                 case 1:
                     move('.section1 h1').scale(1.5).end();
-                    move('.section1 p').set('margin-top','10px').end();
+                    move('.section1 p').set('margin-top','20px').end();
                     var i=0
                     setInterval(function () {
                         i++
@@ -158,17 +22,14 @@ $(document).ready(function(){
                     },1)
                     break;
                 case 2:
-                    move('.section2 h1').scale(1.2).end(function () {
-                        move('.section2 a').scale(1.2).end()
-                        }
-                    );
+                    move('.section2 h1').set('margin-left','200px').end();
+                    move('.section2 .personalInfo').set('margin-top','300px').end();
                     break;
                 case 3:
-                    move('.section3 .imgone').set('margin-top','0px').end(function(){
-                        move('.section3 .imgtwo').set('margin-top','0px').end(function(){
-                            move('.section3 .imgthree').set('margin-top','0px').end();
-                        })
-                    });
+                    move('.section3 .imgone').set('display','block').end()
+                        move('.section3 .imgtwo').set('display','block').end()
+                            move('.section3 .imgthree').set('display','block').end();
+                    
                     break;
                 case 4:
                     move('.section4 .imglist').set('margin-top','30px').end(function(){
@@ -181,24 +42,24 @@ $(document).ready(function(){
                     move('.section5 .encourageone').set('left','43%').end(function(){
                         move('.section5 .encouragetwo').set('left','31%').end(function(){
                             move('.section5  .encouragethree').set('left','56%').end()
-                            });
                         });
+                    });
                     break;
                 case 6:
                     move('.section6 h1').set('margin-top','60px').end(function () {
                         move('.section6 .imgone').set('margin-top','0px').end(function () {
-                                    move('.section6 .imgtwo').set('margin-top','0px').end(function () {
-                                            move('.section6 .imgthree').set('margin-top','0px').end(function () {
-                                    });
-                                })
+                            move('.section6 .imgtwo').set('margin-top','0px').end(function () {
+                                move('.section6 .imgthree').set('margin-top','0px').end(function () {
+                                });
+                            })
                         })
                     });
                     break;
                 case 7:
                     move('.section7 h1').set('margin-top','90px').end(function () {
-                       move('.section7 .joinus').set('margin-top','40px').end(function () {
-                          move('.section7 .submmint').set('margin-top','60px').end()
-                    });
+                        move('.section7 .joinus').set('margin-top','40px').end(function () {
+                            move('.section7 .submmint').set('margin-top','60px').end()
+                        });
                     })
                     break;
                 default :
@@ -213,19 +74,19 @@ $(document).ready(function(){
                     move('.section1 p').set('margin-top','800px').end();
                     break;
                 case 2:
-                    move('.section2 h1').scale(1).end();
-                    move('.section2 a').scale(1).end()
+                    move('.section2 h1').set('margin-left','-200px').end();
+                    move('.section2 .personalInfo').set('margin-top','1200px').end();
                     break;
                 case 3:
-                    move('.section3 .imglist .imgone').set('margin-top','1500px').end();
-                    move('.section3 .imglist .imgtwo').set('margin-top','1500px').end();
-                    move('.section3 .imglist .imgthree').set('margin-top','1500px').end();
+                    move('.section3 .imglist .imgone').set('display','none').end();
+                    move('.section3 .imglist .imgtwo').set('display','none').end();
+                    move('.section3 .imglist .imgthree').set('display','none').end();
                     break;
                 case 4:
                     move('.section4 .imglist').set('margin-top','3000px').end()
                     move('.section4 .ultwo').set('margin-top','1200px').end()
                     move('.section4 .ultwo').scale(1).end()
-                      0
+                    0
                     break;
                 case 5:
                     move('.section5 .encourageone').set('left','-43%').end()
