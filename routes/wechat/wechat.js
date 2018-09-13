f087569c4d8d88127bc4fa80f9381c2210b74863
Xiao Meng <xiaohehe@douyu.tv>
2018-09-13 16:43:21 +0800

    调试代码

--- routes/wechat/wechat.js
+++ routes/wechat/wechat.js
@@ -306,7 +306,7 @@ router.post('/InsertAdvice', function (req, res, next) {
 router.post('/fankuiContent', function (req, res, next) {
   var content=req.body.content
   var phoneNum=req.body.phoneNum
-  Advice.update({resNum:req.body.resNum},{$set:{fankuiContent:content}},{safe:false},function(err, result) {
+  Advice.update({resNum:req.body.resNum},{$set:{fankuiContent:content}},function(err, result) {
       console.log(err)
       console.log(result)
       if (err) {
