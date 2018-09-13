f0817b52ca0ae5a60ef31ef39cb012557bc0d82d
Xiao Meng <xiaohehe@douyu.tv>
2018-09-13 17:49:42 +0800

    调试代码

--- routes/wechat/wechat.js
+++ routes/wechat/wechat.js
@@ -306,37 +306,32 @@ router.post('/InsertAdvice', function (req, res, next) {
 router.post('/fankuiContent', function (req, res, next) {
   var content=req.body.content
   var phoneNum=req.body.phoneNum
-    Advice.findOne({resNum:req.body.resNum},function (err,doc) {
-        Advice.update({resNum:req.body.resNum},{$set:{content:content}},function(err, result) {
-            console.log(err)
-            console.log(result)
-            if (err) {
-                res.end(err)
-            } else {
-                smsUtils.sendNotifySMS_qcloud(phoneNum, smsUtils.code29, [content], function (err) {
-                    if (err) {
-                        console.log(err)
-                        res.end("error")
-                    } else {
-                        // var dataObj={
-                        //   resNum:moment().format('YYYYMMDDHHmmss') + _.random(10000, 99999),
-                        //   resType:req.body.resType,
-                        //   images:req.body.images,
-                        //   contacts:req.body.contacts,
-                        //   phoneNum:req.body.phoneNum,
-                        //   details:req.body.details,
-                        //   detailPlace:req.body.detailPlace
-                        // }
-                        // var advice=new History(dataObj)
-                        //
-                        res.end("success")
-                    }
-                })
-            }
-        })
-
+    Advice.update({resNum:req.body.resNum},{$set:{content:content}},function(err, result) {
+        console.log(result)
+        if (err) {
+            res.end(err)
+        } else {
+            smsUtils.sendNotifySMS_qcloud(phoneNum, smsUtils.code29, [content], function (err) {
+                if (err) {
+                    console.log(err)
+                    res.end("error")
+                } else {
+                    // var dataObj={
+                    //   resNum:moment().format('YYYYMMDDHHmmss') + _.random(10000, 99999),
+                    //   resType:req.body.resType,
+                    //   images:req.body.images,
+                    //   contacts:req.body.contacts,
+                    //   phoneNum:req.body.phoneNum,
+                    //   details:req.body.details,
+                    //   detailPlace:req.body.detailPlace
+                    // }
+                    // var advice=new History(dataObj)
+                    //
+                    res.end("success")
+                }
+            })
+        }
     })
-
 })
 
 router.get('/toExcel',function (req,res,next) {
