b70f39638db65c2b15ccef0cada314d1dbed7bdc
Xiao Meng <xiaohehe@douyu.tv>
2018-09-13 16:50:36 +0800

    调试代码

--- routes/wechat/wechat.js
+++ routes/wechat/wechat.js
@@ -308,32 +308,7 @@ router.post('/fankuiContent', function (req, res, next) {
   var phoneNum=req.body.phoneNum
     Advice.findOne({resNum:req.body.resNum},function (err,doc) {
         doc.fankuiContent=content
-        Advice.update({resNum:req.body.resNum},{$set:doc},function(err, result) {
-            console.log(result)
-            if (err) {
-                res.end(err)
-            } else {
-                smsUtils.sendNotifySMS_qcloud(phoneNum, smsUtils.code29, [content], function (err) {
-                    if (err) {
-                        console.log(err)
-                        res.end("error")
-                    } else {
-                        // var dataObj={
-                        //   resNum:moment().format('YYYYMMDDHHmmss') + _.random(10000, 99999),
-                        //   resType:req.body.resType,
-                        //   images:req.body.images,
-                        //   contacts:req.body.contacts,
-                        //   phoneNum:req.body.phoneNum,
-                        //   details:req.body.details,
-                        //   detailPlace:req.body.detailPlace
-                        // }
-                        // var advice=new History(dataObj)
-                        //
-                        res.end("success")
-                    }
-                })
-            }
-        })
+        doc.save();
 
     })
 })
