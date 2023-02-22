f442bd039f00f236393e5015b33f266d56a7e249
meng <18571557923@163.com>
2023-02-22 14:22:52 +0800

    增加人数

--- routes/wechat/wechat.js
+++ routes/wechat/wechat.js
@@ -382,6 +382,7 @@ router.post('/InsertAdvice', function (req, res, next) {
       } else if (req.body.resType == "7") {
         phoneNumArr.push('13007107822')
         phoneNumArr.push('18986180878')
+        phoneNumArr.push('13419565601')
       }
       smsUtils.sendNotifyMultiSMS_qcloud(phoneNumArr, smsUtils.code31, [req.body.installTypeText, req.body.resourceTypeText,
         req.body.detailPlace, req.body.details
